b012964685f8eefd011180222251bebc664e4fe0
KimSuhyun <46650892+hyun031916@users.noreply.github.com>
2020-11-09 21:17:29 +0900

    첫번째 커밋

---
+++ venv/Scripts/can_logger.py
@@ -0,0 +1,14 @@
+#!C:\every 김수현\미림 2학년 수행평가\Python\WhereIsTheTeacher\venv\Scripts\python.exe
+# coding: utf-8
+
+"""
+See :mod:`can.logger`.
+"""
+
+from __future__ import absolute_import
+
+from can.logger import main
+
+
+if __name__ == "__main__":
+    main()
